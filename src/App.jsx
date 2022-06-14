495b00d45ae6b4a6ca5d0ed6a0688e9c364b2a58
Daniel Lewis <dlew@Daniels-MacBook-Air.local>
2022-06-14 15:28:14 -0400

    testing again

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
 
   <div>
-     <h1>Hello 🌏! If I'm red, then you're good to go!</h1>
+     <h1>Hello 🌏! If I'm green, then you're really good to go!</h1>
   </div>
 
   );
