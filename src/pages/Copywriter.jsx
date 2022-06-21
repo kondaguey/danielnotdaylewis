b282d4e1b4e0e24b425f73ffb809d4695ef65aa7
Daniel Lewis <dlew@macbook-air.myfiosgateway.com>
2022-06-21 11:56:16 -0400

    complete /copywriter

--- src/pages/Copywriter.jsx
+++ src/pages/Copywriter.jsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactAudioPlayer from 'react-audio-player';
+
 
 function Copywriter() {
+
+  const [embedURL] = useState("https://docs.google.com/document/d/e/2PACX-1vSNbfQobuuvkFdfRQLYY04nUAFtjXkzTsaI4CBQ3HBVfnApEJNPb4s5oOPXZ_Sci5FqaScN7mpQGbZb/pub?embedded=true"); 
+
   return (
   <div className="copy">
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
-     <h1>🏗 Big time construction underway </h1>
 
+    <div className="copy__header">
+      <h1 className="copy__header-title typewriter-font">/copywriter</h1>
+      <h2 className="copy__header-subtitle">sample work</h2>
+    </div>
+
+    <div className="copy__document">
+      <iframe title="pdf" src= {embedURL} width="900" height="1150"></iframe>
+    </div>
+
+    <div className="copy__audio">
+
+      <p>. . . being that I'm also a proferssional voice actor, I even threw in a bonus voiceover of the article for the client. On the house.</p>
+      <ReactAudioPlayer className="copy__audio-clip" src="./media/sample-audio.mp3" controls />
+    </div>
+
+    <div className="copy__feedback">
+      <p>client feedback . . .</p>
+      <img src="./img/feedback.png" alt="client feedback" />
+    </div>
 
   </div>
   );
